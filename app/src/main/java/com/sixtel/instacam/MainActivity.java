eb21459f25b5e71ccd6689dff11d39b1718756d6
BrandenSandahl <branden157@gmail.com>
2016-07-06 11:34:17 -0400

    settings things up

--- app/src/main/java/com/sixtel/instacam/MainActivity.java
+++ app/src/main/java/com/sixtel/instacam/MainActivity.java
@@ -1,15 +1,31 @@
 package com.sixtel.instacam;
 
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST = 10;
+    private static final String TAG = "MainActivity";
+    private File mPhoto;
+    private FeedFragment mFeedFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +34,57 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                "android.permission.CAMERA")
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{"android.permission.CAMERA"},
+                    0);
+        }
+
+
+        mFeedFragment = (FeedFragment) getSupportFragmentManager().findFragmentById(R.id.feed_container);
+        if (mFeedFragment == null) {
+            mFeedFragment = new FeedFragment();
+
+            FragmentManager fm = getSupportFragmentManager();
+            fm.beginTransaction()
+                    .add(R.id.feed_container, mFeedFragment)
+                    .commit();
+
+
+        }
+    }
+
+
+
+        public void onClick(View v) {
+            Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            File directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM); //directory path to where photos are stored
+            //random file name
+            mPhoto = new File(directory, "sample.jpeg");
+
+            i.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mPhoto)); //lets the phone know where to save the photo
+
+            startActivityForResult(i, CAMERA_REQUEST);
+
+        }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST) {
+            if (resultCode == RESULT_OK) {
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setDataAndType(Uri.fromFile(mPhoto), "image/jpeg");
+
+                startActivity(i);
+
             }
-        });
+        }
     }
 
     @Override
@@ -49,4 +108,8 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
 }
---
+++ app/src/main/java/com/sixtel/instacam/Photo.java
@@ -0,0 +1,7 @@
+package com.sixtel.instacam;
+
+/**
+ * Created by branden on 7/6/16.
+ */
+public class Photo {
+}
